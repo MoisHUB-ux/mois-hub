a615b414fc87d196ec9c27bb95dd4e81508bcfd2
MoisHUB-ux <rus.mois322@gmail.com>
2025-12-17 16:30:40 +0000

    Update images config to remotePatterns

--- next.config.js
+++ next.config.js
@@ -2,7 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['igkosdwgkvqpucwovrdz.supabase.co'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'igkosdwgkvqpucwovrdz.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
   },
 }
 
